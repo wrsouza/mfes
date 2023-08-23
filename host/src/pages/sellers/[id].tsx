de254d438b23786c65a5404b80ed21ef0ddd1be4
Willian Souza <willian.robert@grupoboticario.com.br>
2023-08-23 09:35:52 -0300

    add edit seller

--- host/src/pages/sellers/[id].tsx
+++ host/src/pages/sellers/[id].tsx
@@ -1,7 +1,19 @@
 import type { GetServerSidePropsContext } from "next";
 import dynamic from "next/dynamic";
 const SellerEditPage = dynamic(() => import("sellers/pages/[id]"), {
-  ssr: false,
+  ssr: true,
 });
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const page = await import("sellers/pages/[id]");
+
+  if (page.getServerSideProps) {
+    return page.getServerSideProps(context);
+  }
+
+  return {
+    props: {},
+  };
+}
 
 export default SellerEditPage;
--- sellers/src/pages/[id].tsx
+++ sellers/src/pages/[id].tsx
@@ -1,15 +1,36 @@
+import { GetServerSideProps, GetServerSidePropsContext } from 'next';
 import Link from "next/link";
-import { useRouter } from "next/router";
 
-export default function EditSeller() {
-  const router = useRouter();
+type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+};
 
+type SellersProps = {
+  data: User;
+};
+
+export default function EditSeller({ data }: SellersProps) {
   return (
     <div className="container">
-      <h1>Edit Seller {router.query.id}</h1>
+      <h1>Edit Seller {data.id}</h1>
+      <div>
+        <p>Name: {data.name}</p>
+        <p>Username: {data.username}</p>
+        <p>E-mail: {data.email}</p>
+      </div>
       <Link href="/sellers" className="btn btn-primary btn-sm">
         Voltar
       </Link>
     </div>
   );
+}
+
+export const getServerSideProps: GetServerSideProps = async (context: GetServerSidePropsContext) => {
+  const { id } = context.query;
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  const data = await res.json();
+  return { props: { data } };
 }
